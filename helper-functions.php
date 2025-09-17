afe95c0b3d7c15f8f433aac89ce328c553fdd55e
Marius Gerum-Richter <marius.gerum@gmail.com>
2025-09-17 15:36:30 +0200

    Added new function to develop branch

--- helper-functions.php
+++ helper-functions.php
@@ -3,3 +3,7 @@
 	function print_hello_world() {
 		echo 'Hello World in develop branch :)';
 	}
+	
+	function some_awesome_new_function(){
+		echo 'This is some awesome new function that only exists in the develop branch. Merge `develop` into `main` and it will be available in the `main` branch too.`';
+	}
